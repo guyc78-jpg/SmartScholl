f470cba33a22ab2c79d49729a4ecb8d8e1fb425f
Guy Cohen <guyc78@gmail.com>
2026-05-26 06:36:30 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -336,6 +336,7 @@ export default function AppLayout({ children, user, role, darkMode, toggleDark, 
           >
             {darkMode ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
           </button>
+          <div className="w-11 h-11" />
         </header>
 
         {/* Page Content */}
